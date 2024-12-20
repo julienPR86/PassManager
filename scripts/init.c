519ae86f1737a499a1127e5755fb0cf011d15e25
JulienPR86 <julien.pr86@gmail.com>
2024-12-20 15:18:08 +0100

    back to the last system

--- scripts/init.c
+++ scripts/init.c
@@ -1,7 +1,7 @@
 #include "../manager.h"
 
 const char *separation = "     ";
-const char *data_path = "passwords.txt";
+char *data_path = "C:\\Users\\neser\\Documents\\Developpement\\C\\PassManager\\test";
 int last_msg_is_error = 0;
 int is_modifiable = 0;
 int running = 1;
@@ -19,7 +19,7 @@ int init()
         return 1;
     }
     get_dimensions(file, &text_height, &text_width);
-    text = read_file(file);
+    text = read_file(file, text_height, text_width);
     if (text == NULL)
     {
         error_msg("Cannot read the file");
