26463cb9427f24f5789cb4390d159b673517fc4c
JulienPR86 <julien.pr86@gmail.com>
2024-12-21 17:17:06 +0100

    change help display if is_modifiable is 1 or 0

--- scripts/help.c
+++ scripts/help.c
@@ -4,13 +4,22 @@ int help(char *command)
 {
     if (command == NULL)
     {
-        printf("\n > add : ADD an identifier and a password with a plateform name you can access with it\n");
-        printf("\n > data : DISPLAY or CHANGE the actual data path\n");
-        printf("\n > exit : EXIT the program\n");
-        printf("\n > get : GET an identifier and a password from a plateform name\n");
-        printf("\n > help : DISPLAY the description of the commands and their function\n");
-        printf("\n > list : DISPLAY all the plateform's names you can access\n");
-        printf("\n > remove : REMOVE an identifier and a password from a plateform name\n\n");
+        if (is_modifiable)
+        {
+            printf("\n > add : ADD an identifier and a password with a plateform name you can access with it\n");
+            printf("\n > data : DISPLAY or CHANGE the actual data path\n");
+            printf("\n > exit : EXIT the program\n");
+            printf("\n > get : GET an identifier and a password from a plateform name\n");
+            printf("\n > help : DISPLAY the description of the commands and their function\n");
+            printf("\n > list : DISPLAY all the plateform's names you can access\n");
+            printf("\n > remove : REMOVE an identifier and a password from a plateform name\n\n");
+        }
+        else
+        {
+            printf("\n > data : DISPLAY or CHANGE the actual data path\n");
+            printf("\n > exit : EXIT the program\n");
+            printf("\n > help : DISPLAY the description of the commands and their function\n\n");
+        }
     }
     else
     {
