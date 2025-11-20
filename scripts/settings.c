b91731ef52a18a12e66d94ca6681ca8a41860aa6
julienPR86 <julien.pr86@gmail.com>
2025-11-20 10:02:16 +0100

    + add change_setting function core

--- scripts/settings.c
+++ scripts/settings.c
@@ -46,7 +46,28 @@ int	get_setting_value_index(char *setting_name)
 
 int	change_setting_value(char *setting_name, char *value)
 {
+	int			setting_index;
+	char		*setting_save;
+	const char	*null_value = "none";
+
 	if (NULL == setting_name)
 		return (FAILURE);
+	setting_index = get_setting_value_index(setting_name);
+	if (-1 == setting_index)
+		return (FAILURE);
+	if (NULL == value)
+		value = (char *)null_value;
+	setting_save = *(settings_file_content + setting_index);
+	*(settings_file_content + setting_index) = (char *)malloc(sizeof(char) * (strlen(setting_name) + strlen(value) + 2));
+	if (NULL == *(settings_file_content + setting_index))
+	{
+		*(settings_file_content + setting_index) = setting_save;
+		return (FAILURE);
+	}
+	free(setting_save);
+	strcpy(*(settings_file_content + setting_index), setting_name);
+	strcat(*(settings_file_content + setting_index), " ");
+	strcat(*(settings_file_content + setting_index), value);
+	rewrite_settings_file = 1;
 	return (SUCCESS);
 }
