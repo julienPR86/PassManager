2e7138f041ae10b4e60016481d858f0faaa19fd0
julienPR86 <julien.pr86@gmail.com>
2025-11-20 20:52:43 +0100

    # uppdated init function

--- scripts/init.c
+++ scripts/init.c
@@ -7,40 +7,35 @@ char	**settings_file_content;
 
 int	init(void)
 {
-	FILE	*data_file;
-	FILE	*settings_file;
 	char	*data_file_path;
 
 	rewrite_data_file = 0;
 	rewrite_settings_file = 0;
 	data_file_content = NULL;
 	settings_file_content = NULL;
-	settings_file = fopen(SETTINGS_PATH, "r");
-	if (NULL == settings_file)
-		return (COULD_NOT_OPEN_SETTINGS_FILE);
-	settings_file_content = read_file(settings_file);
-	if (NULL == settings_file_content)
+	switch (get_file_content(SETTINGS_PATH, &settings_file_content))
 	{
-		fclose(settings_file);
-		return (FAILED_TO_READ_SETTINGS_FILE);
+		case COULD_NOT_OPEN_FILE :
+			return (COULD_NOT_OPEN_SETTINGS_FILE);
+		case FAILED_TO_READ_FILE :
+			return (FAILED_TO_READ_SETTINGS_FILE);
+		default :
+			break;
 	}
-	fclose(settings_file);
 	data_file_path = get_setting_value("data_path");
-	data_file = fopen(data_file_path, "r");
-	if (NULL == data_file)
+	switch (get_file_content(data_file_path, &data_file_content))
 	{
-		free(data_file_path);
-		return (COULD_NOT_OPEN_FILE);
+		case COULD_NOT_OPEN_FILE :
+			free(data_file_path);
+			return (COULD_NOT_OPEN_FILE);
+		case FAILED_TO_READ_FILE :
+			free(data_file_path);
+			free_strings(settings_file_content);
+			return (FAILED_TO_READ_FILE);
+		default :
+			break;
 	}
 	free(data_file_path);
-	data_file_content = read_file(data_file);
-	if (NULL == data_file_content)
-	{
-		fclose(data_file);
-		free_strings(settings_file_content);
-		return (FAILED_TO_READ_FILE);
-	}
-	fclose(data_file);
 	sort_strings(data_file_content);
 	return (SUCCESS);
 }
