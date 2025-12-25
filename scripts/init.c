fc4623b52f2cce8342aa6c049ae284c0b72e81c0
julienPR86 <julien.pr86@gmail.com>
2025-12-25 16:28:15 +0100

    + added history file content initalisation at program initialisation

--- scripts/init.c
+++ scripts/init.c
@@ -2,17 +2,22 @@
 
 int		rewrite_data_file;
 int		rewrite_settings_file;
+int		rewrite_history_file;
 char	**data_file_content;
 char	**settings_file_content;
+char	**history_file_content;
 
 int	init(void)
 {
 	char	*data_file_path;
+	char	*history_file_path;
 
 	rewrite_data_file = 0;
 	rewrite_settings_file = 0;
+	rewrite_history_file = 0;
 	data_file_content = NULL;
 	settings_file_content = NULL;
+	history_file_content = NULL;
 	switch (get_file_content(SETTINGS_PATH, &settings_file_content))
 	{
 		case COULD_NOT_OPEN_FILE :
@@ -23,6 +28,8 @@ int	init(void)
 			break;
 	}
 	data_file_path = get_setting_value("data_path");
+	if (NULL == data_file_path)
+		return (SETTING_NOT_FOUND);
 	switch (get_file_content(data_file_path, &data_file_content))
 	{
 		case COULD_NOT_OPEN_FILE :
@@ -36,6 +43,18 @@ int	init(void)
 			break;
 	}
 	free(data_file_path);
+	history_file_path = get_setting_value("history_path");
+	if (NULL == history_file_path)
+		return (SETTING_NOT_FOUND);
+	switch (get_file_content(history_file_path, &history_file_content))
+	{
+		case COULD_NOT_OPEN_FILE :
+			message_output("No history available\n");
+			break ;
+		default :
+			break ;
+	}
+	free(history_file_path);
 	srand(time(NULL) * (strings_size(data_file_content) + 1));
 	sort_strings(data_file_content);
 	return (SUCCESS);
