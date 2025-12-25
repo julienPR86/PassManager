427e2094fd6e65be0b2ad1d9dfbcb9091ce4a8b4
julienPR86 <julien.pr86@gmail.com>
2025-12-25 22:15:38 +0100

    + added update_history function

---
+++ scripts/history.c
@@ -0,0 +1,35 @@
+#include "../headers/manager.h"
+
+int	update_history(char	**args)
+{
+	const char	*separation = " ";
+	char		*line;
+	t_uint		index;
+	t_uint		size;
+
+	if (NULL == args)
+		return (FAILURE);
+	size = 0;
+	index = 0;
+	while (*(args + index))
+	{
+		size += strlen(*(args + index));
+		++index;
+	}
+	line = (char *)malloc(sizeof(char) * (size + (strings_size(args) - 1) * strlen(separation) + 1));
+	if (NULL == line)
+		return (FAILURE);
+	*line = '\0';
+	index = 0;
+	while (*(args + index))
+	{
+		strcat(line, *(args + index));
+		if (*(args + index + 1))
+			strcat(line, separation);
+		++index;
+	}
+	*(line + size + strings_size(args) - 1) = '\0';
+	strs_add_line(&history_file_content, line);
+	free(line);
+	return (SUCCESS);
+}
