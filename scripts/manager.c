1f597f24b7a3338a0a40e5d5b504f040589ccc93
julienPR86 <julien.pr86@gmail.com>
2025-12-26 09:05:01 +0100

    # updated message outputs

--- scripts/manager.c
+++ scripts/manager.c
@@ -30,20 +30,20 @@ int	manager(void)
 	{
 		data_file_path = get_setting_value("data_path");
 		if (SUCCESS != rewrite_file(data_file_path, data_file_content))
-			error_output("Failed to rewrite data file\n");
+			message_output(ERROR, "Failed to rewrite data file.");
 		free(data_file_path);
 	}
 	if (rewrite_history_file)
 	{
 		history_file_path = get_setting_value("history_path");
 		if (SUCCESS != rewrite_file(history_file_path, history_file_content))
-			error_output("Failed to rewrite history file\n");
+			message_output(ERROR, "Failed to rewrite history file.");
 		free(history_file_path);
 	}
 	if (rewrite_settings_file)
 	{
 		if (SUCCESS != rewrite_file(SETTINGS_PATH, settings_file_content))
-			error_output("Failed to rewrite settings file\n");
+			message_output(ERROR, "Failed to rewrite settings file.");
 	}
 	return (SUCCESS);
 }
@@ -53,19 +53,12 @@ int	main(void)
 	switch (init())
 	{
 		case INITIALISATION_FAILED:
-			error_output("Could not initialise the program\n");
 			return (FAILURE);
 		case COULD_NOT_OPEN_SETTINGS_FILE:
-			error_output("Could not open settings file\n");
 			return (FAILURE);
 		case FAILED_TO_READ_SETTINGS_FILE:
-			error_output("Failed to read the settings file\n");
 			return (FAILURE);
-		case COULD_NOT_OPEN_FILE:
-			error_output("Could not open data file\n");
-			break;
 		case FAILED_TO_READ_FILE:
-			error_output("Failed to read content file\n");
 			return (FAILURE);
 		default:
 			break;
