9b930cbd8a3e102e6648b93cd467ccdb53ea0373
julienPR86 <julien.pr86@gmail.com>
2026-09-12 21:30:58 +0200

    refractor: simplified update_history definition

--- sources/history.c
+++ sources/history.c
@@ -4,31 +4,12 @@ int	update_history(char	**args)
 {
 	const char	*separation = " ";
 	char		*line;
-	t_uint		index;
-	t_uint		size;
 
 	if (NULL == args)
 		return (FAILURE);
-	size = 0;
-	index = 0;
-	while (*(args + index))
-	{
-		size += strLength(*(args + index));
-		++index;
-	}
-	line = (char *)malloc(sizeof(char) * (size + (strings_size(args) - 1) * strLength(separation) + 1));
+	line = strJoin((const char **)args, separation);
 	if (NULL == line)
 		return (FAILURE);
-	*line = '\0';
-	index = 0;
-	while (*(args + index))
-	{
-		strcat(line, *(args + index));
-		if (*(args + index + 1))
-			strcat(line, separation);
-		++index;
-	}
-	*(line + size + strings_size(args) - 1) = '\0';
 	if (!strCompare(*args, "add"))
 		replace_word(line, 3, "\t ", '*');
 	else if (!strCompare(*args, "replace"))
